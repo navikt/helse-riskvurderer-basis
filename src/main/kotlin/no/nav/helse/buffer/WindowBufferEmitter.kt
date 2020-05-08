16985f341abc9ab67e578f20ace5180a504eebc9
Roy A Gundersen <roy.andre.gundersen@nav.no>
2020-05-08 14:14:30 +0200

    sessionEarlyExpireCondition

--- src/main/kotlin/no/nav/helse/buffer/WindowBufferEmitter.kt
+++ src/main/kotlin/no/nav/helse/buffer/WindowBufferEmitter.kt
@@ -8,77 +8,96 @@ import java.util.concurrent.Executors
 import java.util.concurrent.TimeUnit
 
 interface ExpiredSession<K, V> {
-   val key: K
-   val values: List<V>
-   fun delete()
+    val key: K
+    val values: List<V>
+    fun delete()
 }
 
-class MySessionStore<K, V> {
+class MySessionStore<K, V>(private val sessionEarlyExpireCondition: ((List<V>) -> Boolean)?) {
 
-   private val data: MutableMap<K, MutableMap<UUID, Sess<V>>> = mutableMapOf()
+    private val data: MutableMap<K, MutableMap<UUID, Sess<V>>> = mutableMapOf()
 
-   private inner class MyExpiredSession(override val key: K, val session: Sess<V>) : ExpiredSession<K, V> {
-      override val values: List<V>
-         get() = session.recs
+    private inner class MyExpiredSession(override val key: K, val session: Sess<V>) : ExpiredSession<K, V> {
+        override val values: List<V>
+            get() = session.recs
 
-      override fun delete() {
-         data[key]?.let {
-            it.remove(session.id)
-            synchronized(it) {
-               if (it.isEmpty()) {
-                  data.remove(key)
-               }
+        override fun delete() {
+            data[key]?.let {
+                it.remove(session.id)
+                synchronized(it) {
+                    if (it.isEmpty()) {
+                        data.remove(key)
+                    }
+                }
             }
-         }
-      }
-   }
+        }
+    }
 
-   private class Sess<T>(timestamp: Long) {
-      val id = UUID.randomUUID()
-      val intialTimestamp: Long = timestamp
-      var lastActiveTimestamp: Long = timestamp
-      val recs: MutableList<T> = mutableListOf()
-   }
+    private class Sess<T>(timestamp: Long) {
+        val id = UUID.randomUUID()
+        val intialTimestamp: Long = timestamp
+        var lastActiveTimestamp: Long = timestamp
+        val recs: MutableList<T> = mutableListOf()
+        var expiredEarlyByCondition: Boolean = false
+    }
 
-   private var sessionMaxAgeVAR: Long = Duration.ofMinutes(30).toMillis()
-   var sessionMaxAgeMs: Long
-      get() = if (sessionMaxAgeVAR > sessionGapMs) sessionMaxAgeVAR else sessionGapMs
-      set(value) {
-         if (value < sessionGapMs) throw IllegalArgumentException("maxAgeMs cannot be smaller than sessionGapMs")
-         sessionMaxAgeVAR = value
-      }
+    private var sessionMaxAgeVAR: Long = Duration.ofMinutes(30).toMillis()
+    var sessionMaxAgeMs: Long
+        get() = if (sessionMaxAgeVAR > sessionGapMs) sessionMaxAgeVAR else sessionGapMs
+        set(value) {
+            if (value < sessionGapMs) throw IllegalArgumentException("maxAgeMs cannot be smaller than sessionGapMs")
+            sessionMaxAgeVAR = value
+        }
 
-   var sessionGapMs: Long = Duration.ofSeconds(5).toMillis()
+    var sessionGapMs: Long = Duration.ofSeconds(5).toMillis()
 
-   private fun Sess<V>.hasExpiredBy(timestamp: Long) =
-      (timestamp - this.lastActiveTimestamp > sessionGapMs) ||
-         (timestamp - this.intialTimestamp > sessionMaxAgeMs)
+    private fun Sess<V>.hasExpiredBy(timestamp: Long) =
+        (timestamp - this.lastActiveTimestamp > sessionGapMs) ||
+            (timestamp - this.intialTimestamp > sessionMaxAgeMs) ||
+            this.expiredEarlyByCondition
 
+    private fun Sess<V>.addToSession(value: V, timestamp: Long) {
+        this.recs += value
+        if (sessionEarlyExpireCondition?.let { it(recs) } ?: false) {
+            this.expiredEarlyByCondition = true
+        }
+        this.lastActiveTimestamp = timestamp
+    }
 
-   private fun Sess<V>.addToSession(value: V, timestamp: Long) {
-      this.recs += value
-      this.lastActiveTimestamp = timestamp
-   }
+    fun set(key: K, value: V, timestamp: Long) {
+        set(key, value, timestamp, false)
+    }
 
-   fun set(key: K, value: V, timestamp: Long) {
-      val sessions = data[key] ?: mutableMapOf<UUID, Sess<V>>().apply { data[key] = this }
-      synchronized(sessions) {
-         val activeSession = sessions.values.lastOrNull().let {
-            if (it == null || it.hasExpiredBy(timestamp))
-               Sess<V>(timestamp).also { newSession -> sessions[newSession.id] = newSession }
-            else it
-         }
-         activeSession.addToSession(value, timestamp)
-      }
-   }
+    fun setAndReturnSessionOnEarlyExpiry(key: K, value: V, timestamp: Long): ExpiredSession<K, V>? {
+        return set(key, value, timestamp, true)
+    }
 
-   fun allExpiredSessions(currentTime: Long): List<ExpiredSession<K, V>> =
-      data.keys.flatMap { allExpiredSessionsForKey(it, currentTime) }
+    private fun set(key: K, value: V, timestamp: Long, checkExpiry: Boolean): MyExpiredSession? {
+        val sessions = data[key] ?: mutableMapOf<UUID, Sess<V>>().apply { data[key] = this }
+        synchronized(sessions) {
+            val activeSession = sessions.values.lastOrNull().let {
+                if (it == null || it.hasExpiredBy(timestamp))
+                    Sess<V>(timestamp).also { newSession -> sessions[newSession.id] = newSession }
+                else it
+            }
+            activeSession.addToSession(value, timestamp)
+            if (activeSession.expiredEarlyByCondition && checkExpiry) {
+                return MyExpiredSession(key, activeSession)
+            }
+        }
+        return null
+    }
 
-   fun allExpiredSessionsForKey(key: K, currentTime: Long): List<ExpiredSession<K, V>> =
-      data[key]?.values?.filter { it.hasExpiredBy(currentTime) }?.map {
-         MyExpiredSession(key, it)
-      } ?: emptyList()
+    fun allExpiredSessions(currentTime: Long, ignoreEarlyExpiredSessions: Boolean = false): List<ExpiredSession<K, V>> =
+        data.keys.flatMap { allExpiredSessionsForKey(it, currentTime, ignoreEarlyExpiredSessions) }
+
+    fun allExpiredSessionsForKey(key: K, currentTime: Long, ignoreEarlyExpiredSessions: Boolean): List<ExpiredSession<K, V>> =
+        data[key]?.values?.filter {
+            it.hasExpiredBy(currentTime)
+                && !(ignoreEarlyExpiredSessions && it.expiredEarlyByCondition)
+        }?.map {
+            MyExpiredSession(key, it)
+        } ?: emptyList()
 
 }
 
@@ -86,38 +105,52 @@ class WindowBufferEmitter(private val windowSizeInSeconds: Long,
                           private val aggregateAndEmit: (List<JsonObject>) -> Unit,
                           private val clock: Clock = Clock.systemDefaultZone(),
                           scheduleExpiryCheck: Boolean = true,
-                          schedulerIntervalInSeconds:Long = windowSizeInSeconds) {
+                          schedulerIntervalInSeconds: Long = windowSizeInSeconds,
+                          sessionEarlyExpireCondition: ((List<JsonObject>) -> Boolean)? = null) {
 
-   private val scheduler = Executors.newSingleThreadScheduledExecutor()
-   private var runningExpiryCheck = false
+    private val scheduler = Executors.newSingleThreadScheduledExecutor()
+    private var runningExpiryCheck = false
+    val earlyExpiryEnabled: Boolean = sessionEarlyExpireCondition != null
 
-   init {
-      if (scheduleExpiryCheck) {
-         scheduler.scheduleAtFixedRate({
-            runExpiryCheck()
-         }, schedulerIntervalInSeconds, schedulerIntervalInSeconds, TimeUnit.SECONDS)
-      }
-   }
+    init {
+        if (scheduleExpiryCheck) {
+            scheduler.scheduleAtFixedRate({
+                runExpiryCheck()
+            }, schedulerIntervalInSeconds, schedulerIntervalInSeconds, TimeUnit.SECONDS)
+        }
+    }
 
-   internal fun runExpiryCheck() {
-      try {
-         if (!runningExpiryCheck) {
-            runningExpiryCheck = true
-            store.allExpiredSessions(clock.millis()).forEach {
-               aggregateAndEmit(it.values)
-               it.delete()
+    internal fun runExpiryCheck() {
+        try {
+            if (!runningExpiryCheck) {
+                runningExpiryCheck = true
+                store.allExpiredSessions(
+                    currentTime = clock.millis(),
+                    // We don't want to emit result both here in scheduler and in "earlyEmitter":
+                    ignoreEarlyExpiredSessions = earlyExpiryEnabled
+                ).forEach {
+                    aggregateAndEmit(it.values)
+                    it.delete()
+                }
             }
-         }
-      } finally {
-         runningExpiryCheck = false
-      }
-   }
+        } finally {
+            runningExpiryCheck = false
+        }
+    }
 
-   private val store = MySessionStore<String, JsonObject>().apply {
-      sessionGapMs = windowSizeInSeconds * 1000
-   }
+    private val store = MySessionStore<String, JsonObject>(sessionEarlyExpireCondition).apply {
+        sessionGapMs = windowSizeInSeconds * 1000
+    }
 
-   fun store(key: String, value: JsonObject, timestamp: Long) {
-      store.set(key, value, timestamp)
-   }
+    fun store(key: String, value: JsonObject, timestamp: Long) {
+        if (earlyExpiryEnabled) {
+            val earlyExpiredSession = store.setAndReturnSessionOnEarlyExpiry(key, value, timestamp)
+            if (earlyExpiredSession != null) {
+                aggregateAndEmit(earlyExpiredSession.values)
+                earlyExpiredSession.delete()
+            }
+        } else {
+            store.set(key, value, timestamp)
+        }
+    }
 }
--- src/test/kotlin/no/nav/helse/buffer/WindowBufferEmitterTest.kt
+++ src/test/kotlin/no/nav/helse/buffer/WindowBufferEmitterTest.kt
@@ -5,63 +5,132 @@ import io.mockk.mockk
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.json.JsonObject
 import kotlinx.serialization.json.content
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import java.time.Clock
 import kotlin.test.assertEquals
 
 class WindowBufferEmitterTest {
 
-   @Test
-   fun `split in sessions based on windowGap`() {
-      val opinion1a = """{"vedtaksperiodeId":"periode1", "type": "vurdering", "vekt":2, "score": 4, "begrunnelser": ["reason 1", "reason 2"]}"""
-      val opinion1b = """{"vedtaksperiodeId":"periode1", "type": "vurdering", "vekt":10, "score": 10, "begrunnelser": ["reason 1", "reason 2"]}"""
-      val opinion2 = """{"vedtaksperiodeId":"periode2", "type": "vurdering", "vekt":3, "score": 5, "begrunnelser": ["reason 3"]}"""
-      val o1a = Json.parse(JsonObject.serializer(), opinion1a)
-      val o1b = Json.parse(JsonObject.serializer(), opinion1b)
-      val o2 = Json.parse(JsonObject.serializer(), opinion2)
+    @Test
+    fun `split in sessions based on windowGap`() {
+        val opinion1a = """{"vedtaksperiodeId":"periode1", "type": "vurdering", "vekt":2, "score": 4, "begrunnelser": ["reason 1", "reason 2"]}"""
+        val opinion1b = """{"vedtaksperiodeId":"periode1", "type": "vurdering", "vekt":10, "score": 10, "begrunnelser": ["reason 1", "reason 2"]}"""
+        val opinion2 = """{"vedtaksperiodeId":"periode2", "type": "vurdering", "vekt":3, "score": 5, "begrunnelser": ["reason 3"]}"""
+        val o1a = Json.parse(JsonObject.serializer(), opinion1a)
+        val o1b = Json.parse(JsonObject.serializer(), opinion1b)
+        val o2 = Json.parse(JsonObject.serializer(), opinion2)
 
-      val sessions = mutableListOf<List<JsonObject>>()
-      fun lagOgSendVurdering(msgs: List<JsonObject>) {
-         sessions += msgs
-         println("laOgSend: $msgs")
-      }
+        val sessions = mutableListOf<List<JsonObject>>()
+        fun lagOgSendVurdering(msgs: List<JsonObject>) {
+            sessions += msgs
+            println("laOgSend: $msgs")
+        }
 
-      val clock = mockk<Clock>()
+        val clock = mockk<Clock>()
 
-      every { clock.millis() } returns 10000
+        every { clock.millis() } returns 10000
 
-      val window = WindowBufferEmitter(20, ::lagOgSendVurdering, clock, false)
-      window.store("periode1", o1a, 1000)
-      window.store("periode1", o1b, 12000)
-      window.store("periode1", o1b, 18000)
+        val window = WindowBufferEmitter(20, ::lagOgSendVurdering, clock, false)
+        window.store("periode1", o1a, 1000)
+        window.store("periode1", o1b, 12000)
+        window.store("periode1", o1b, 18000)
 
-      window.store("periode2", o2, 7000)
-      window.store("periode2", o2, 17000)
+        window.store("periode2", o2, 7000)
+        window.store("periode2", o2, 17000)
 
-      every { clock.millis() } returns 36999
-      window.runExpiryCheck()
-      assertEquals(0, sessions.size)
+        every { clock.millis() } returns 36999
+        window.runExpiryCheck()
+        assertEquals(0, sessions.size)
 
-      window.store("periode1", o1a, 38001)
+        window.store("periode1", o1a, 38001)
 
-      every { clock.millis() } returns 60000
-      window.runExpiryCheck()
-      assertEquals(3, sessions.size)
-      sessions[0].apply {
-         assertEquals(3, this.size)
-         for (i in 0..2) {
-            assertEquals("periode1", this[i]["vedtaksperiodeId"]?.content)
-         }
-      }
-      sessions[1].apply {
-         assertEquals(1, this.size)
-         assertEquals("periode1", this.first()["vedtaksperiodeId"]?.content)
-      }
-      sessions[2].apply {
-         assertEquals(2, this.size)
-         for (i in 0..1) {
-            assertEquals("periode2", this[i]["vedtaksperiodeId"]?.content)
-         }
-      }
-   }
+        every { clock.millis() } returns 60000
+        window.runExpiryCheck()
+        assertEquals(3, sessions.size)
+        sessions[0].apply {
+            assertEquals(3, this.size)
+            for (i in 0..2) {
+                assertEquals("periode1", this[i]["vedtaksperiodeId"]?.content)
+            }
+        }
+        sessions[1].apply {
+            assertEquals(1, this.size)
+            assertEquals("periode1", this.first()["vedtaksperiodeId"]?.content)
+        }
+        sessions[2].apply {
+            assertEquals(2, this.size)
+            for (i in 0..1) {
+                assertEquals("periode2", this[i]["vedtaksperiodeId"]?.content)
+            }
+        }
+    }
+
+
+    @Test
+    fun `early expiry`() {
+        val opinion1a = """{"vedtaksperiodeId":"periode1", "type": "vurdering", "infotype":"A", "vekt":2, "score": 4, "begrunnelser": ["reason 1", "reason 2"]}"""
+        val opinion1b = """{"vedtaksperiodeId":"periode1", "type": "vurdering", "infotype":"B", "vekt":10, "score": 10, "begrunnelser": ["reason 1", "reason 2"]}"""
+        val o1a = Json.parse(JsonObject.serializer(), opinion1a)
+        val o1b = Json.parse(JsonObject.serializer(), opinion1b)
+
+        val sessions = mutableListOf<List<JsonObject>>()
+        fun lagOgSendVurdering(msgs: List<JsonObject>) {
+            sessions += msgs
+            println("laOgSend: $msgs")
+        }
+
+        val clock = mockk<Clock>()
+
+        every { clock.millis() } returns 1000
+        var current = clock.millis()
+
+        val windowSizeInSecs = 20L
+        val windowSizeInMillis = windowSizeInSecs * 1000
+
+        val window = WindowBufferEmitter(
+            windowSizeInSeconds = windowSizeInSecs,
+            aggregateAndEmit = ::lagOgSendVurdering,
+            clock = clock,
+            scheduleExpiryCheck = false,
+            schedulerIntervalInSeconds = -1,
+            sessionEarlyExpireCondition = {
+                it.size == 2
+                    && (it.find { msg -> msg["infotype"]?.content == "A" } != null)
+                    && (it.find { msg -> msg["infotype"]?.content == "B" } != null)
+            })
+        window.store("periode1", o1a, 1000)
+        window.store("periode1", o1b, 1000)
+        assertEquals(1, sessions.size, "should have been emitted early because of earlyExpireCondition")
+        sessions[0].apply {
+            assertEquals(2, this.size)
+            assertTrue(this.contains(o1a))
+            assertTrue(this.contains(o1b))
+        }
+        sessions.clear()
+        every { clock.millis() } returns current + 2000
+        current = clock.millis()
+
+        window.runExpiryCheck()
+        assertEquals(0, sessions.size, "should not be emitted again")
+        every { clock.millis() } returns current + windowSizeInMillis + 1000
+        current = clock.millis()
+
+        window.runExpiryCheck()
+        assertEquals(0, sessions.size, "should not be emitted again, even after window-expiry")
+
+        window.store("periode1", o1a, clock.millis())
+        every { clock.millis() } returns current + windowSizeInMillis + 1000
+        current = clock.millis()
+
+        window.runExpiryCheck()
+        assertEquals(1, sessions.size)
+        sessions[0].apply {
+            assertEquals(1, this.size, "should be emitted after windowExpiry, even if earlyExpiryCondition is not met")
+            assertTrue(this.contains(o1a))
+        }
+
+    }
+
+
 }

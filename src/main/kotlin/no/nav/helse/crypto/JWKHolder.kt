dc766c3786c66ba409f8fee63bef2a200504d3d5
Roy A Gundersen <roy.andre.gundersen@nav.no>
2020-12-07 13:42:42 +0100

    convenience-metoder for å laste JWK/JWKS

--- src/main/kotlin/no/nav/helse/crypto/JWKHolder.kt
+++ src/main/kotlin/no/nav/helse/crypto/JWKHolder.kt
@@ -2,13 +2,41 @@ package no.nav.helse.crypto
 
 import com.nimbusds.jose.jwk.JWK
 import com.nimbusds.jose.jwk.JWKSet
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.Paths
+
+private val defaultSecretBasePath: Path = Paths.get("/var/run/secrets")
+private val defaultVaultBasePath: Path = Paths.get("/var/run/secrets/nais.io/vault")
 
 interface JWKHolder {
     fun jwk() : JWK
+    companion object {
+        fun fromDynamicFile(filepath: Path): JWKHolder = DynamicJWKFromPath(filepath)
+        fun fromSecret(filename: String): JWKHolder = fromDynamicFile(defaultSecretBasePath.resolve(filename))
+        fun fromVault(filename: String): JWKHolder = fromDynamicFile(defaultVaultBasePath.resolve(filename))
+    }
 }
 
 interface JWKSetHolder {
     fun jwkSet() : JWKSet
+    companion object {
+        fun fromDynamicFile(filepath: Path): JWKSetHolder = DynamicJWKSetFromPath(filepath)
+        fun fromSecret(filename: String): JWKSetHolder = fromDynamicFile(defaultSecretBasePath.resolve(filename))
+        fun fromVault(filename: String): JWKSetHolder = fromDynamicFile(defaultVaultBasePath.resolve(filename))
+        fun fromMultiple(vararg holders: JWKSetHolder) : JWKSetHolder = MultiJWKSetHolder(holders)
+
+    }
+}
+
+internal class DynamicJWKFromPath(val filepath: Path) : JWKHolder {
+    override fun jwk(): JWK = JWK.parse(Files.readString(filepath))
+}
+internal class DynamicJWKSetFromPath(val filepath: Path) : JWKSetHolder {
+    override fun jwkSet(): JWKSet = JWKSet.parse(Files.readString(filepath))
+}
+internal class MultiJWKSetHolder(private val holders: Array<out JWKSetHolder>) : JWKSetHolder {
+    override fun jwkSet(): JWKSet = JWKSet(holders.flatMap { it.jwkSet().keys })
 }
 
 fun JWK.toJWKHolder() : JWKHolder =
---
+++ src/test/kotlin/no/nav/helse/crypto/JWKHolderTest.kt
@@ -0,0 +1,120 @@
+package no.nav.helse.crypto
+
+import com.nimbusds.jose.jwk.JWKSet
+import org.junit.jupiter.api.Test
+import java.nio.charset.Charset
+import java.nio.file.*
+import java.util.*
+import kotlinx.serialization.json.*
+import org.junit.jupiter.api.assertThrows
+import kotlin.test.assertEquals
+
+class JWKHolderTest {
+
+    val tempDir = System.getProperty("java.io.tmpdir")
+    val tempDirPath = Paths.get(tempDir)
+
+    @Test
+    fun `JWK and JWKS is updated when files are updated on disk`() {
+        val jwk1 = lagEnJWK("key-1")
+        val jwks1 = JWKSet(jwk1)
+        val jsonJWK1 = jwk1.toJSONObject().toString()
+        val jsonJWKS1 = jwks1.toJSONObject(false).toString()
+
+        val jwkPath: Path = tempDirPath.resolve(UUID.randomUUID().toString())
+        val jwksPath: Path = tempDirPath.resolve(UUID.randomUUID().toString())
+
+        jsonJWK1.writeToFile(jwkPath)
+        jsonJWKS1.writeToFile(jwksPath)
+
+        val jwk2 = lagEnJWK("key-2")
+        val jwks1and2 = JWKSet(listOf(jwk1, jwk2))
+        val jsonJWK2 = jwk2.toJSONObject().toString()
+        val jsonJWKS1and2 = jwks1and2.toJSONObject(false).toString()
+
+        val jwkHolder = JWKHolder.fromDynamicFile(jwkPath)
+        val jwksHolder = JWKSetHolder.fromDynamicFile(jwksPath)
+
+        val payload1 = json {
+            "felt" to "verdi"
+        }
+
+        val encrypted1 = payload1.encryptAsJWE(jwkHolder)
+        assertEquals(payload1, JsonElement.decryptFromJWE(encrypted1, jwksHolder))
+
+        jsonJWK2.writeToFile(jwkPath) // Overwriting JWK referred by jwkHolder
+        val encrypted2 = payload1.encryptAsJWE(jwkHolder)
+        assertThrows<RuntimeException>("jwksHolder should no longer contain correct key") {
+            JsonElement.decryptFromJWE(encrypted2, jwksHolder)
+        }
+
+        jsonJWKS1and2.writeToFile(jwksPath) // Write new JWKS with both key-1 and key-2 to disk
+        assertEquals(payload1, JsonElement.decryptFromJWE(encrypted2, jwksHolder), "we should be able to decrypt encrypted2 with updated JWKS")
+        assertEquals(payload1, JsonElement.decryptFromJWE(encrypted1, jwksHolder), "we should also be able to decrypt OLD encrypted1 with updated JWKS")
+    }
+
+    @Test
+    fun `JWKSet from multiple other JWKSets`() {
+        val jwk1 = lagEnJWK("key-1")
+        val jwks1 = JWKSet(jwk1)
+        val jsonJWK1 = jwk1.toJSONObject().toString()
+        val jsonJWKS1 = jwks1.toJSONObject(false).toString()
+        val jwk1Path: Path = tempDirPath.resolve(UUID.randomUUID().toString())
+        val jwks1Path: Path = tempDirPath.resolve(UUID.randomUUID().toString())
+        jsonJWK1.writeToFile(jwk1Path)
+        jsonJWKS1.writeToFile(jwks1Path)
+
+        val jwk2 = lagEnJWK("key-2")
+        val jwks2 = JWKSet(jwk2)
+        val jsonJWK2 = jwk2.toJSONObject().toString()
+        val jsonJWKS2 = jwks2.toJSONObject(false).toString()
+        val jwk2Path: Path = tempDirPath.resolve(UUID.randomUUID().toString())
+        val jwks2Path: Path = tempDirPath.resolve(UUID.randomUUID().toString())
+        jsonJWK2.writeToFile(jwk2Path)
+        jsonJWKS2.writeToFile(jwks2Path)
+
+        val jwk1Holder = JWKHolder.fromDynamicFile(jwk1Path)
+        val jwks1Holder = JWKSetHolder.fromDynamicFile(jwks1Path)
+
+        val jwk2Holder = JWKHolder.fromDynamicFile(jwk2Path)
+        val jwks2Holder = JWKSetHolder.fromDynamicFile(jwks2Path)
+
+        val jwksALLHolder = JWKSetHolder.fromMultiple(
+            JWKSetHolder.fromDynamicFile(jwks1Path),
+            JWKSetHolder.fromDynamicFile(jwks2Path)
+        )
+
+        val payload1 = json { "felt" to "verdi" }
+        val payload2 = json { "felt" to "verdi-nummer2" }
+
+        val encrypted1 = payload1.encryptAsJWE(jwk1Holder)
+        val encrypted2 = payload2.encryptAsJWE(jwk2Holder)
+
+        assertEquals(payload1, JsonElement.decryptFromJWE(encrypted1, jwks1Holder))
+        assertThrows<RuntimeException> { JsonElement.decryptFromJWE(encrypted1, jwks2Holder )}
+        assertEquals(payload1, JsonElement.decryptFromJWE(encrypted1, jwksALLHolder))
+
+        assertThrows<RuntimeException> { JsonElement.decryptFromJWE(encrypted2, jwks1Holder )}
+        assertEquals(payload2, JsonElement.decryptFromJWE(encrypted2, jwks2Holder))
+        assertEquals(payload2, JsonElement.decryptFromJWE(encrypted2, jwksALLHolder))
+
+        val payload3 = json { "felt" to "verdi-nummer-TRE" }
+
+        val jwk3 = lagEnJWK("key-3")
+        val jwks1and3 = JWKSet(listOf(jwk1, jwk3))
+        val jwk3Holder = jwk3.toJWKHolder()
+        val jsonJWKS1and3 = jwks1and3.toJSONObject(false).toString()
+
+        val encrypted3 = payload3.encryptAsJWE(jwk3Holder)
+        assertThrows<RuntimeException> { JsonElement.decryptFromJWE(encrypted3, jwksALLHolder )}
+        jsonJWKS1and3.writeToFile(jwks1Path) // Writing new JWKS to jwks-1-path should fix also the "multi-holder":
+        assertEquals(payload3, JsonElement.decryptFromJWE(encrypted3, jwksALLHolder), "ALL-holder whould get updated key-3 from jwks1Path")
+        assertEquals(payload2, JsonElement.decryptFromJWE(encrypted2, jwksALLHolder), "key-2 should still work")
+        assertEquals(payload1, JsonElement.decryptFromJWE(encrypted1, jwksALLHolder), "and key-1 should also still work")
+    }
+
+    private fun String.writeToFile(path: Path) {
+        Files.writeString(path, this, Charset.defaultCharset(), StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)
+    }
+
+}
